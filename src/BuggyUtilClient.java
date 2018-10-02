2196b3ec521c0897c24a5bfb7aa45d2fd3af7cb7
nextinfinity <xgix57@gmail.com>
2018-10-02 14:49:28 -0400

    Commit comment

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -1,5 +1,6 @@
 
 public class BuggyUtilClient {
+    //Random comment to demo committing through IntelliJ
     public static void main(String[] args) {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
